00381a9d5032ec4a6d3ab6b4b7fc249579977fdf
suzuki_kiwamu <suzuki_kiwamu@cyberagent.co.jp>
2018-10-11 15:30:52 +0900

    first commit

---
+++ sampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampleApp
+//
+//  Created by suzuki_kiwamu on 2018/10/10.
+//  Copyright © 2018年 kiwamunet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
